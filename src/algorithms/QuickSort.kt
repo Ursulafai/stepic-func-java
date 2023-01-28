db1aa396db69bb596927a772735bf143e56a077b
Ursulafai <tashanegodyaeva@gmail.com>
2023-01-28 19:20:55 +0300

    algo - add random pivot value

--- src/algorithms/QuickSort.kt
+++ src/algorithms/QuickSort.kt
@@ -1,32 +1,37 @@
 package algorithms
 
+import kotlin.random.Random
+
+
 object QuickSort {
     @JvmStatic
     fun main(args: Array<String>) {
         val initialArray = intArrayOf(3, 4, 1, 11, 10, 34, 5, 6, 9)
-        quickSort(initialArray, initialArray[0], initialArray.lastIndex)
+        println(initialArray.contentToString())
+        quickSort(initialArray, 0, initialArray.lastIndex)
+        println(initialArray.contentToString())
     }
 
     private fun partitioning(intArray: IntArray, left: Int, right: Int): Int {
-//        val randomIndex = Random().nextInt(right - 1)
-        val pivot = intArray[right]
+        random(intArray, left, right)
+        val pivotValue: Int = intArray[right]
 
         var i = left - 1 // Index of smaller element and indicates the right position of pivot found so far
 
         for (j in left until right) {
 
             // If current element is smaller than the pivot
-            if (intArray[j] < pivot) {
+            if (intArray[j] < pivotValue) {
                 i++   // increment index of smaller element
                 swap(intArray, j, i)
             }
         }
         swap(intArray, i + 1, right)
-        return (i + 1)
+        return i + 1
     }
 
     private fun quickSort(intArray: IntArray, left: Int, right: Int) {
-        if (left > right) {
+        if (left >= right) {
             return
         }
 
@@ -43,5 +48,12 @@ object QuickSort {
 
         intArray[left] = intArray[right]
         intArray[right] = tmp
+    }
+
+    private fun random(intArray: IntArray, left: Int, right: Int) {
+        val rand = Random
+        val pivot = rand.nextInt(right - left + left)
+
+        swap(intArray, right, pivot)
     }
 }
