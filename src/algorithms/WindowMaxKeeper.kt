cdc798abe1b1040ce6a61eafe3869fec8a490806
Ursulafai <tashanegodyaeva@gmail.com>
2023-02-01 22:02:29 +0300

    algo - find max in the window of array

---
+++ src/algorithms/WindowMaxKeeper.kt
@@ -0,0 +1,23 @@
+package algorithms
+
+object WindowMaxKeeper {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val array = intArrayOf(4, 8, 1, 7, 0)
+        findMaxInWindow(array, 2)
+    }
+
+    private fun findMaxInWindow(array: IntArray, windowSize: Int) {
+        for (i in 0..array.size - windowSize) {
+            var max = array[i]
+            for (j in i until i + windowSize - 1) {
+
+                if (array[j + 1] > max) {
+                    max = array[j + 1]
+                }
+            }
+
+            println("$max")
+        }
+    }
+}
